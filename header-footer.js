1b0000d06aff3487baf401a8c9aa72baf0d1802e
khoakdn <khoakdnguyen@gmail.com>
2024-11-13 09:01:55 +0100

    1-a commit

--- header-footer.js
+++ header-footer.js
@@ -4,7 +4,6 @@ export const DEFHEADER=(...args)=>HEADER(
         DIV({class: 'navbar-links'},
             A({href: 'docs.html'}, 'Docs'),
             ...args,
-            A({href: '#'}, 'API'),
             A({href: '#'}, 'Playground'),
             IMG({src: 'github-icon.png', alt: 'GitHub', class: 'icon'})
         ),
