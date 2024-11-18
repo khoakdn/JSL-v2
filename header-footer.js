33da396f6bac9a2dec8c282c3a6aafd0a20fdeb3
khoakdn <khoakdnguyen@gmail.com>
2024-11-18 14:12:05 +0100

    4th commit
    Homepage, docs page.

--- header-footer.js
+++ header-footer.js
@@ -1,12 +1,12 @@
 export const DEFHEADER=(...args)=>HEADER(
     NAV({class: 'navbar'},
-        DIV({class: 'navbar-logo'}, A({href: 'index.html'}, 'JSL Logo')),
-        INPUT({type: 'text', placeholder: 'Search...', class: 'search-bar'}),
+        DIV({class: 'navbar-logo'}, A(IMG({src: './images/jsl3-logo.png'}), {href: 'index.html'}), 'JSL3'),
+        INPUT({type: 'text', placeholder: 'Start typing...', class: 'search-bar'}),
         DIV({class: 'navbar-links'},
             A({href: 'docs.html'}, 'Docs'),
             // ...args,
             A({href: '#'}, 'Playground'),
-            IMG({src: 'github-icon.png', alt: 'GitHub', class: 'icon'})
+            A({href: 'https://github.com', target: 'blank'}, IMG({src: './images/github.png', alt: 'blank', class: 'icon'}))
         )
     )
 );
